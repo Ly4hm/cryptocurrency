3d0c83b5366ea3a873e632f73d39cd477ba45a9f
Ly4hm <550653451@qq.com>
2024-09-20 17:17:45 +0800

    +1

--- task1/utils.py
+++ task1/utils.py
@@ -24,8 +24,10 @@ class SignatureMachine:
             message_and_signature 参数格式：<message>:<signature>
     """
 
-    def __init__(self) -> None:
-        self.private_key, self.public_key = SignatureMachine._generate_rsa_keypair()
+    def __init__(self, key_size=2048) -> None:
+        self.key_size = key_size
+        self.private_key, self.public_key = self._generate_rsa_keypair()
+        self.r = None
 
     def sign(self, message: str) -> str:
         "对给定的消息进行rsa盲签名"
@@ -36,11 +38,11 @@ class SignatureMachine:
         "签名验证，true 验证通过，反之失败"
         return self._verify_signature(message_and_signature)
 
-    def _generate_rsa_keypair():
+    def _generate_rsa_keypair(self):
         "生成RSA密钥对"
         private_key = rsa.generate_private_key(
             public_exponent=65537,
-            key_size=2048,
+            key_size=self.key_size,
         )
         public_key = private_key.public_key()
 
@@ -51,7 +53,7 @@ class SignatureMachine:
         message_hash = int.from_bytes(
             hashlib.sha256(message.encode()).digest(), byteorder="big"
         )
-        r = (
+        self.r = (
             int.from_bytes(os.urandom(32), byteorder="big")
             % self.public_key.public_numbers().n
         )
@@ -59,7 +61,7 @@ class SignatureMachine:
         # 公式：(r^e * m) % n
         blinded_message = (
             pow(
-                r,
+                self.r,
                 self.public_key.public_numbers().e,
                 self.public_key.public_numbers().n,
             )
@@ -79,26 +81,34 @@ class SignatureMachine:
         )
         return int.from_bytes(blinded_signature, byteorder="big")
 
+    def _unblind_signature(self, blinded_signature):
+        n = self.public_key.public_numbers().n
+        # 计算r的模逆元
+        r_inv = pow(self.r, -1, n)
+        # 计算去盲化后的签名
+        signature = (blinded_signature * r_inv) % n
+        return signature
+
     def _verify_signature(self, message_and_signature) -> bool:
         "验证签名"
         # 解码消息
-        message, signature = message_and_signature.split(":")
+        message, blinded_signature = message_and_signature.split(":")
         message = base64.b64decode(message).decode("utf-8")
+        signature = self._unblind_signature(int(blinded_signature))
 
         message_hash = int.from_bytes(
             hashlib.sha256(message.encode()).digest(), byteorder="big"
         )
         recovered_hash = pow(
-            int(signature),
+            signature,
             self.public_key.public_numbers().e,
             self.public_key.public_numbers().n,
         )
 
-        print(message_hash)
         print(recovered_hash)
-        print(self.public_key.public_numbers().n)
+        print(self.sign(message))
 
-        return recovered_hash == message_hash
+        return recovered_hash == self.sign(message)
 
 
 if __name__ == "__main__":
@@ -106,6 +116,6 @@ if __name__ == "__main__":
     message = "hello world"
     message_signed = signature_machine.sign(message)
 
-    print(message_signed)
+    # print(message_signed)
 
-    # print(signature_machine.verify(message_signed))
+    print(signature_machine.verify(message_signed))
